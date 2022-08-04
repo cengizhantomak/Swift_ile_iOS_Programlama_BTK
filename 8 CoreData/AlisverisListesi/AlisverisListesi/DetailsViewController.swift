cfd959424ee347bc472a5317fc80a7be9b91f626
Cengizhan Tomak <cengizhantomak@gmail.com>
2022-08-04 20:13:14 +0300

    Veri Kaydetmek

--- 8 CoreData/AlisverisListesi/AlisverisListesi/DetailsViewController.swift
+++ 8 CoreData/AlisverisListesi/AlisverisListesi/DetailsViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -45,6 +46,32 @@ class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, 
     }
     
     @IBAction func kaydetButtonTiklandi(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let alisveris = NSEntityDescription.insertNewObject(forEntityName: "Alisveris", into: context)
+        
+        alisveris.setValue(isimTextField.text, forKey: "isim")
+        alisveris.setValue(bedenTextField.text, forKey: "beden")
+        
+        if let fiyat = Int(fiyatTextField.text!) {
+            alisveris.setValue(fiyat, forKey: "fiyat")
+        }
+        
+        //universal unique id
+        alisveris.setValue(UUID(), forKey: "id")
+        
+        let data = imageView.image!.jpegData(compressionQuality: 0.5)
+        
+        alisveris.setValue(data, forKey: "gorsel")
+        
+        do {
+            try context.save()
+            print("kayıt edildi")
+        } catch {
+            print("hata var")
+        }
+        
         
     }
     
