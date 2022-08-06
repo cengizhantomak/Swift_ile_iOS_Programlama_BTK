b4e31519a6e2489f8be254196df1670294272e3e
Cengizhan Tomak <cengizhantomak@gmail.com>
2022-08-06 12:02:45 +0300

    7 Kullanici Arayuzu

---
+++ 9 Haritalar/HaritalarUygulamasi/HaritalarUygulamasi/ListViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ListViewController.swift
+//  HaritalarUygulamasi
+//
+//  Created by Tuna Tomak on 6.08.2022.
+//
+
+import UIKit
+
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(artiButtonuTiklandi))
+    }
+    
+    @objc func artiButtonuTiklandi() {
+        performSegue(withIdentifier: "toMapsVC", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "text"
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
